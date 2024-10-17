e2b449c971f88920aa1cf7ed708b31a7dae58758
S D the fucking G <140980492+megadeglitcher@users.noreply.github.com>
2024-10-17 23:59:10 +0300

    asd

--- RSDKv4/Drawing.cpp
+++ RSDKv4/Drawing.cpp
@@ -3676,7 +3676,7 @@ void DrawSpriteRotozoom(int direction, int XPos, int YPos, int pivotX, int pivot
 }
 
 void DrawSpriteAllEffect(int direction, int XPos, int YPos, int pivotX, int pivotY, int sprX, int sprY, int width, int height, int rotation, int scale,
-                        int sheetID, int alpha, int ink, int flags)
+                        int sheetID, int alpha, int ink, int flags, int scaleX, int scaleY)
 {
 #if RETRO_SOFTWARE_RENDER
     int roundedYPos = 0;
