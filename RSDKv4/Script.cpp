76714cdad67941b30a37a9d5bdf8d25c9a3beb45
The Nexus <138070802+EggBanana123@users.noreply.github.com>
2024-11-29 20:32:53 +0000

    Update Script.cpp

--- RSDKv4/Script.cpp
+++ RSDKv4/Script.cpp
@@ -5575,7 +5575,8 @@ void ProcessScript(int scriptCodePtr, int jumpTablePtr, byte scriptEvent)
 #elif RETRO_PLATFORM == RETRO_WIN // the stink
 				strcpy(temporarTheSecond, "https://");
 				strcat(temporarTheSecond, scriptText);
-				ShellExecuteA(NULL,"start",temporarTheSecond.c_str(),NULL,NULL,SW_SHOW);
+				std::string websit = temporarTheSecond
+				ShellExecuteA(NULL,"start",websit.c_str(),NULL,NULL,SW_SHOW);
 #endif
                 break;
             }
