ecc25854190ac4bd6c3ad189edf0548932585d61
S D the fucking G <140980492+megadeglitcher@users.noreply.github.com>
2024-11-29 14:53:23 +0200

    greyscale

--- RSDKv4/Script.cpp
+++ RSDKv4/Script.cpp
@@ -4725,10 +4725,17 @@ void ProcessScript(int scriptCodePtr, int jumpTablePtr, byte scriptEvent)
             case FUNC_DRAWSPRITEFX:
                 opcodeSize  = 0;
                 spriteFrame = &scriptFrames[scriptInfo->frameListOffset + scriptEng.operands[0]];
-                DrawSpriteAllEffect(entity, entity->direction, (scriptEng.operands[2] >> 16) - xScrollOffset,
-                                           (scriptEng.operands[3] >> 16) - yScrollOffset, -spriteFrame->pivotX, -spriteFrame->pivotY,
-                                           spriteFrame->sprX, spriteFrame->sprY, spriteFrame->width, spriteFrame->height, entity->rotation,
-                                           entity->scale, scriptInfo->spriteSheetID, entity->alpha, entity->inkEffect, scriptEng.operands[1]);
+				if (entity->inkEffect == 7) {
+					DrawScaledTintMask(entity->direction, scriptEng.operands[2], scriptEng.operands[3], -spriteFrame->pivotX,
+									   -spriteFrame->pivotY, entity->scale, entity->scale, spriteFrame->width, spriteFrame->height,
+									   spriteFrame->sprX, spriteFrame->sprY, scriptInfo->spriteSheetID);
+				}
+				else {
+					DrawSpriteAllEffect(entity, entity->direction, (scriptEng.operands[2] >> 16) - xScrollOffset,
+											   (scriptEng.operands[3] >> 16) - yScrollOffset, -spriteFrame->pivotX, -spriteFrame->pivotY,
+											   spriteFrame->sprX, spriteFrame->sprY, spriteFrame->width, spriteFrame->height, entity->rotation,
+											   entity->scale, scriptInfo->spriteSheetID, entity->alpha, entity->inkEffect, scriptEng.operands[1]);
+				}	
                 break;
                 /*
                 switch (scriptEng.operands[1]) {
@@ -4840,10 +4847,18 @@ void ProcessScript(int scriptCodePtr, int jumpTablePtr, byte scriptEvent)
                 */
             case FUNC_DRAWSPRITESCREENFX:
                 opcodeSize  = 0;
-                spriteFrame = &scriptFrames[scriptInfo->frameListOffset + scriptEng.operands[0]];
-                DrawSpriteAllEffect(entity, entity->direction, scriptEng.operands[2], scriptEng.operands[3], -spriteFrame->pivotX,
-                                           -spriteFrame->pivotY, spriteFrame->sprX, spriteFrame->sprY, spriteFrame->width, spriteFrame->height,
-                                           entity->rotation, entity->scale, scriptInfo->spriteSheetID, entity->alpha, entity->inkEffect, scriptEng.operands[1]);
+                spriteFrame = &scriptFrames[scriptInfo->frameListOffset + scriptEng.operands[0]];		   
+				if (entity->inkEffect == 7) {
+					DrawScaledTintMask(entity->direction, (scriptEng.operands[2] >> 16) - xScrollOffset,
+									   (scriptEng.operands[3] >> 16) - yScrollOffset, -spriteFrame->pivotX, -spriteFrame->pivotY,
+									   entity->scale, entity->scale, spriteFrame->width, spriteFrame->height, spriteFrame->sprX,
+									   spriteFrame->sprY, scriptInfo->spriteSheetID);
+				}
+				else {
+					DrawSpriteAllEffect(entity, entity->direction, scriptEng.operands[2], scriptEng.operands[3], -spriteFrame->pivotX,
+											   -spriteFrame->pivotY, spriteFrame->sprX, spriteFrame->sprY, spriteFrame->width, spriteFrame->height,
+											   entity->rotation, entity->scale, scriptInfo->spriteSheetID, entity->alpha, entity->inkEffect, scriptEng.operands[1]);
+				}				   
                 break;
                 /*
                 switch (scriptEng.operands[1]) {
