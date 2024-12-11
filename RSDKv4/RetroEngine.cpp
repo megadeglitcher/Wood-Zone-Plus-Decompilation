4303b361a8aae8b456a079cab900ad78a592cab2
S D the fucking G <140980492+megadeglitcher@users.noreply.github.com>
2024-12-11 20:21:32 +0200

    GetScreenDirection

--- RSDKv4/RetroEngine.cpp
+++ RSDKv4/RetroEngine.cpp
@@ -1239,6 +1239,7 @@ bool RetroEngine::LoadGameConfig(const char *filePath)
     AddNativeFunction("OpenWebsite", OpenWebsite);
     AddNativeFunction("GetUnixTimestamp", GetUnixTimestamp);
     AddNativeFunction("GetLocalUnixTimestamp", GetLocalUnixTimestamp);
+    AddNativeFunction("GetScreenDirection", GetScreenDirection);
 #endif
 
 #if !RETRO_USE_ORIGINAL_CODE
