6f69dfee444398bc42cda9e86ae7dff254901ecc
S D the fucking G <140980492+megadeglitcher@users.noreply.github.com>
2024-12-12 23:53:27 +0200

    ,

--- RSDKv4/Drawing.cpp
+++ RSDKv4/Drawing.cpp
@@ -371,7 +371,7 @@ void FlipScreen()
             scale =
                 std::fminf(std::floor((float)Engine.windowXSize / (float)SCREEN_XSIZE), std::floor((float)Engine.windowYSize / (float)SCREEN_YSIZE));
         }
-        SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear"); // set interpolation to linear
+        SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"); // set interpolation to linear
         // create texture that's integer scaled.
         texTarget = SDL_CreateTexture(Engine.renderer, SDL_PIXELFORMAT_RGB565, SDL_TEXTUREACCESS_TARGET, SCREEN_XSIZE * scale, SCREEN_YSIZE * scale);
 
