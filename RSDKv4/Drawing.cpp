a67ddd54f0a2c14c23e4205bd86edbf526fead7b
S D the fucking G <140980492+megadeglitcher@users.noreply.github.com>
2024-12-13 18:52:11 +0200

    just making sure

--- RSDKv4/Drawing.cpp
+++ RSDKv4/Drawing.cpp
@@ -64,11 +64,7 @@ int InitRenderDevice()
 
     SDL_DisableScreenSaver();
 
-	if (Engine.scalingMode2 == 0) {
-		SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
-	} else {
-		SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "bilinear");
-	}
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "bilinear");
     SDL_SetHint(SDL_HINT_RENDER_VSYNC, Engine.vsync ? "1" : "0");
 
     byte flags = 0;
@@ -375,11 +371,7 @@ void FlipScreen()
             scale =
                 std::fminf(std::floor((float)Engine.windowXSize / (float)SCREEN_XSIZE), std::floor((float)Engine.windowYSize / (float)SCREEN_YSIZE));
         }
-		if (Engine.scalingMode2 == 0) {
-			SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
-		} else {
-			SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "bilinear");
-		}
+        SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "bilinear"); // set interpolation to linear
         // create texture that's integer scaled.
         texTarget = SDL_CreateTexture(Engine.renderer, SDL_PIXELFORMAT_RGB565, SDL_TEXTUREACCESS_TARGET, SCREEN_XSIZE * scale, SCREEN_YSIZE * scale);
 
@@ -504,11 +496,7 @@ void FlipScreen()
         SDL_RenderPresent(Engine.renderer);
         // reset everything just in case
         SDL_RenderSetLogicalSize(Engine.renderer, SCREEN_XSIZE, SCREEN_YSIZE);
-		if (Engine.scalingMode2 == 0) {
-			SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");
-		} else {
-			SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "bilinear");
-		}
+        SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "bilinear");
         // putting some FLEX TAPE  on that memory leak
         SDL_DestroyTexture(texTarget);
     }
